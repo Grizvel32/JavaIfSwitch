b69d675dd9b173c63e2efafb4cc45877e23c9e48
Grizvel32 <grizenkov1996@gmail.com>
2021-05-12 20:57:41 +0300

    written code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        //вводится символ
+        //1.определить буква ли это (английские)
+        //1.1.если это буква определить маленькая или большая
+//        2 если это не буква то определить цифра ли это
+//        2.1 если цифра то чётная или нечётная
+//        3. иначе вывести неопознанный символ
+
+        char symbol;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("input symbol ");
+        symbol = input.next().charAt(0);
+
+        if (symbol >= 'a' && symbol <= 'z' || symbol >= 'A' && symbol <= 'Z') {
+
+            System.out.println("this is letter");
+            if (symbol >= 'a' && symbol <= 'z') {
+
+                System.out.println("small");
+            } else {
+                System.out.println("big");
+            }
+
+        } else if (symbol >= '0' && symbol <= '9') {
+            System.out.println("this is digit");
+        } else {
+            System.out.println("this is unknown");
+        }
+
 
     }
 }
